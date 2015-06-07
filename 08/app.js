6860c546ee99a747b77ef7e4ca73ac5a82892d23
Kim Junho <kjh44004@naver.com>
2015-06-07 19:27:47 +0900

    카드게임
    카드게임

--- 08/app.js
+++ 08/app.js
@@ -11,43 +11,75 @@ console.log(cards)
 //깔기
 var doms = [];
 for(var i=0; i < cards.length; i++){
-
-  doms.push('<div class="card'+i+'">' + cards[i]+ '</div>');
+	doms.push('<div class="card'+i+'">' + cards[i] + '</div>');
 }
-
+$board.css("text-indent","-9999px"); /*글자를 숨김*/
 $board.html(doms.join(''));
 
+//시간제한
+var $countdown = $('#countdown');
+var time = 5;
+$countdown.html("TIME : " + time);
+
+var timeattack = setInterval(function(){
+
+	time--;
+
+	$countdown.html("TIME : " + time);
+	console.log("TIME : " + time);
+
+	if(time === 0){
+		clearInterval(timeattack);
+		opencount = 2; /*더이상 열리지 않도록 한다.*/
+		alert("fail");
+		$board.addClass("fail");
+		$board.css("text-indent","0px"); /*숨겼던 글자를 보여줌*/
+	}
+}, 1000);
+
+//클릭 이벤트
 var finish = 0;
-var opencount = 0;
+var opencount = -1;
 var remember = [];
 var card_remember = [];
 
 $board.on('click', 'div', function(event){
 
-	var $card = $(event.currentTarget);
+	opencount++; console.log(opencount);
 
-	remember[opencount] = $card; /*console.log(remember[opencount]);*/
+	if(opencount < 2){ /*두개이상은 열리지 않도록 한다.*/
 
-	card_remember[opencount] = $card.html(); console.log(card_remember[opencount]);
+		var $card = $(event.currentTarget);
 
-	$card.addClass("open");
+		remember[opencount] = $card; console.log(remember[opencount]);
 
-	opencount++;
+		card_remember[opencount] = $card.html(); console.log(card_remember[opencount]);
 
-	if(opencount === 2){
-		if(card_remember[0] === card_remember[1]){
-			alert("success!");			
-		}
-		else{
-			alert("fail!");
-			remember[0].removeClass('open');
-			remember[1].removeClass('open');			
-		}
-		opencount = 0;
+		$card.addClass("open");
 
-		if(finish === cards.length){
-			alert("clear!");
+		
+		finish++;
+
+		if(opencount === 1){
+			if(card_remember[0] === card_remember[1]){
+				/*alert("success!");*/				
+				remember[0].addClass('success');
+				remember[1].addClass('success');
+				opencount = -1;				
+			}
+			else{
+				/*alert("fail!");*/
+				setTimeout(function(){
+					remember[0].removeClass('open');
+					remember[1].removeClass('open');			
+					finish = finish - 2;
+					opencount = -1;
+				}, 500);				
+			}
+			if(finish === cards.length){
+				clearInterval(timeattack);
+				alert("clear!");
+			}
 		}
 	}
 });
-
